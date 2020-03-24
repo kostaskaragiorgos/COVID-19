6d8893a63fbd7de901735f21adf54eee8196e536
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-03-24 11:01:04 +0200

    Update Convid19.py

--- Convid19.py
+++ Convid19.py
@@ -2,7 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 df = pd.read_csv('23_3/covid_19_clean_complete.csv')
+print(len(df))
 print(df.dtypes)
+df['Country/Region']= df['Country/Region'].astype("string")
+df = df.drop_duplicates(subset='Country/Region', keep='last')
+print(len(df))
 print("Number of Deaths:",df['Deaths'].sum())
 print("Number of Confirmed:", df['Confirmed'].sum())
 print("Number of Recovered:", df['Recovered'].sum())
@@ -10,3 +14,6 @@ data = [df['Deaths'].sum(), df['Confirmed'].sum(), df['Recovered'].sum()]
 plt.bar(np.arange(3), data)
 plt.xticks(np.arange(3), ('Deaths', 'Confirmed', 'Recovered'))
 plt.show()
+print(df[df['Country/Region']=="Greece"]['Confirmed'].sum())
+print(df[df['Country/Region']=="Greece"]['Deaths'].sum())
+print(df[df['Country/Region']=="Greece"]['Recovered'].sum())
