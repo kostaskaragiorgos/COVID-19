c3850132bf5ad4b376f0803ae6432ae9f5b769b8
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-03-30 23:55:33 +0300

    Update Convid19.py

--- Convid19.py
+++ Convid19.py
@@ -5,12 +5,12 @@ df = pd.read_csv('23_3/covid_19_clean_complete.csv')
 print(len(df))
 print(df.dtypes)
 df['Country/Region']= df['Country/Region'].astype("string")
-df = df.drop_duplicates(subset='Country/Region', keep='last')
+#df = df.drop_duplicates(subset='Country/Region', keep='last')
+"""
 print(len(df))
 print("Number of Deaths:",df['Deaths'].sum())
 print("Number of Confirmed:", df['Confirmed'].sum())
 print("Number of Recovered:", df['Recovered'].sum())
-"""
 data = [df['Deaths'].sum(), df['Confirmed'].sum(), df['Recovered'].sum()]
 plt.bar(np.arange(3), data)
 plt.xticks(np.arange(3), ('Deaths', 'Confirmed', 'Recovered'))
@@ -21,12 +21,6 @@ print(df[df['Country/Region']=="Greece"]['Recovered'].sum())
 """
 
 
-print(df.loc[(df['Country/Region']=="Greece") &(df['Date']=='3/22/20')])
-
-import datetime
-day = datetime.date(2020,3,20).strftime('%x')
-print(day.year)
-month = datetime.date(2020,3,20).month
-day = datetime.date(2020,3,20).day
-
-print(df.loc[(df['Country/Region']=="Greece") &(df['Date']==str(str(month)+'/'+str(day)+'/'+str(year)))])
+from_d = df.loc[(df['Country/Region']=="Greece") &(df['Date']=='3/1/20')]['Deaths']
+to_d = df.loc[(df['Country/Region']=="Greece") &(df['Date']=='3/22/20')]['Deaths']
+print(abs(int(from_d)-int(to_d)))
